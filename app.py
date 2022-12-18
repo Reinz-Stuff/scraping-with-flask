c8a828d71f531b7ac5b612738df3f6a6257ca84f
Fauzi kurniawan <ozilpym123@gmail.com>
2022-12-18 08:51:26 +0700

    login/save session and logout/delete session

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "ThisIsMySecrectKey2022"
 
 
 @app.route("/")
@@ -24,6 +25,42 @@ def myabout():
 @app.route("/contact")
 def mycontact():
     return render_template("contact.html")
+
+
+# Parsing nilai Int, String
+@app.route("/parsing/<string:value>")
+def myparsing(value):
+    return f"Nilainya adalah: {value}"
+
+
+# Argument parser
+@app.route("/parsingargument")
+def parsingargument():
+    data = request.args.get("values")
+    return f"Nilai argument parsernya adalah: {data}"
+
+
+# parsing nilai url untuk men set nilai variable
+@app.route("/page/<int:value>")
+def session_1(value):
+    session["myvalue"] = value
+    return "Successfully set value...!"
+
+
+@app.route("/page/view")
+def view_session():
+    try:
+        data = session["myvalue"]
+        return f"the value that has been set is: {data}"
+    except KeyError:
+        return "No values ​​ are stored...."
+
+
+# logout / delete session
+@app.route("/page/logout")
+def logout_session_1():
+    session.pop("myvalue")
+    return "successfully logout session...."
 
 
 if __name__ == '__main__':
