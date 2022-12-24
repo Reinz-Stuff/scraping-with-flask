8d61c8dfab98c5deed52c92b720f2a1167ec3d9e
Fauzi kurniawan <ozilpym123@gmail.com>
2022-12-24 11:12:47 +0700

    redirect and url_for

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "ThisIsMySecrectKey2022"
@@ -30,14 +30,14 @@ def mycontact():
 # Parsing nilai Int, String
 @app.route("/parsing/<string:value>")
 def myparsing(value):
-    return f"Nilainya adalah: {value}"
+    return f"The Value is: {value}"
 
 
 # Argument parser
 @app.route("/parsingargument")
 def parsingargument():
     data = request.args.get("values")
-    return f"Nilai argument parsernya adalah: {data}"
+    return f"The value of the parser argument is: {data}"
 
 
 # parsing nilai url untuk men set nilai variable
@@ -61,6 +61,17 @@ def view_session():
 def logout_session_1():
     session.pop("myvalue")
     return "successfully logout session...."
+
+
+# Redirect session
+@app.route("/redirect-about")
+def lets_redirect_about():
+    return redirect(url_for('myabout'))
+
+
+@app.route("/redirect-contact")
+def lets_redirect_about():
+    return redirect(url_for('mycontact'))
 
 
 if __name__ == '__main__':
